622e00528854cfe728d7ab1179a281b2a2cec2e0
Giovanni Flores <geflores5@gmail.com>
2020-05-13 13:10:59 -0700

    initial commit

---
+++ src/components/AddNoteForm.js
@@ -0,0 +1,32 @@
+import React, { useContext, useState } from 'react'
+
+import NotesContext from '../context/notes-context'
+import useMousePosition from '../hooks/useMousePosition'
+
+const AddNoteForm = () => {
+  const { dispatch } = useContext(NotesContext)
+  const [title, setTitle] = useState('')
+  const [body, setBody] = useState('')
+  const position = useMousePosition()
+
+  const addNote = (e) => {
+    e.preventDefault()
+    dispatch({ type: 'ADD_NOTE', title, body })
+    setTitle('')
+    setBody('')
+  }
+  return (
+    <React.Fragment>
+      <p>
+        Add Note {position.x} - {position.y}{' '}
+      </p>
+      <form onSubmit={addNote}>
+        <input value={title} onChange={(e) => setTitle(e.target.value)} />
+        <textarea value={body} onChange={(e) => setBody(e.target.value)} />
+        <button>Add Note</button>
+      </form>
+    </React.Fragment>
+  )
+}
+
+export default AddNoteForm
